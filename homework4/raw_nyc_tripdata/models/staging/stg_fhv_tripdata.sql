753b4e0c97edb9c266acc30a9b74999f9aa839f1
chuihsia1997@gmail.com <chuihsia1997@gmail.com>
2025-02-23 21:29:17 +0000

    create core model dim_fhv_trips.sql

--- homework4/raw_nyc_tripdata/models/staging/stg_fhv_tripdata.sql
+++ homework4/raw_nyc_tripdata/models/staging/stg_fhv_tripdata.sql
@@ -1,24 +1,24 @@
-with 
+{{
+    config(
+        materialized='view'
+    )
+}}
 
-source as (
-
-    select * from {{ source('staging', 'external_fhv_tripdata') }}
-
-),
-
-renamed as (
-
-    select
+with fhv_tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging', 'external_fhv_tripdata') }}
+  where dispatching_base_num is not null 
+)
+select
         dispatching_base_num,
-        pickup_datetime,
-        dropoff_datetime,
-        pulocationid,
-        dolocationid,
+        cast(pickup_datetime as timestamp) as pickup_datetime,
+        cast(dropoff_datetime as timestamp) as dropoff_datetime,
+        {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
+        {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
         sr_flag,
         affiliated_base_number
 
-    from source
-
-)
-
-select * from renamed
+from fhv_tripdata
+where rn = 1
